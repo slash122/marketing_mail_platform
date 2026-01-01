5be030b7f33e514876f0c073a4ae533c03d73ce1
Gleb Avrutin <gleb.avrutin@gmail.com>
2026-01-01 19:35:59 +0100

    Add migration on startup for mail server

--- backend/mail_server/src/db_connectors/postgres_connector.py
+++ backend/mail_server/src/db_connectors/postgres_connector.py
@@ -1,7 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
 from src.app_settings import app_settings
 from src.models import MailPostgres
+from sqlalchemy import create_engine
 
 class PostgresConnector:
     def __init__(self):
@@ -9,6 +11,11 @@ class PostgresConnector:
         self.async_session = sessionmaker(
             self.engine, class_=AsyncSession, expire_on_commit=False
         )
+        if app_settings.INIT_DB_ON_STARTUP: self.init_db()
+
+    def init_db(self):
+        sync_engine = create_engine(f"postgresql+psycopg2://{app_settings.POSTGRES_CONNECTION_STRING.get_secret_value()}")
+        SQLModel.metadata.create_all(sync_engine)
 
     async def save_email(self, mail: MailPostgres):
         async with self.async_session() as session:
