0ec4ad077cc795f658708c045679264815bb8920
Gleb Avrutin <gleb.avrutin@gmail.com>
2026-01-02 17:36:49 +0100

    Implement db connector

---
+++ backend/marketing_api/src/db.py
@@ -0,0 +1,30 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from src.app_settings import app_settings
+
+def create_db_url(connection_string: str) -> str:
+    return f"postgresql+asyncpg://{connection_string}"
+
+engine = create_async_engine(
+    create_db_url(app_settings.POSTGRES_CONNECTION_STRING.get_secret_value()), 
+    echo=True, 
+    future=True
+)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
