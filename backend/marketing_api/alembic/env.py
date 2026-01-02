b65a8560b04fac8972a08f107f8b837fede0de88
Gleb Avrutin <gleb.avrutin@gmail.com>
2026-01-02 17:38:09 +0100

    Create base alembic migrations

---
+++ backend/marketing_api/alembic/env.py
@@ -0,0 +1,52 @@
+import asyncio
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+from src.models import Account, Sender, SenderAccountLink, Mail, User, MailState  # noqa: F401
+from sqlalchemy.engine import Connection
+from src.db import engine
+from sqlmodel import SQLModel
+
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = SQLModel.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = engine
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations) 
+        await connectable.dispose()
+
+asyncio.run(run_migrations_online())
